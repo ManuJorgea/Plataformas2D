c08b27b5a4d574fe8dac7707d43d18a5cee1988b
ManuJorgea <69644681+ManuJorgea@users.noreply.github.com>
2024-03-05 19:39:52 -0500

    Collider de personaje y piso

---
+++ Assets/UdeM/Controllers/Character2D.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UdeM.Controllers
+{
+    public class Character2D : EntityController
+    {
+        protected Rigidbody2D _rb2d;
+
+        protected override void Start() {
+            base.Start();
+            _rb2d = GetComponent<Rigidbody2D>();
+            if ( _rb2d == null ) {
+                _rb2d = gameObject.AddComponent<Rigidbody2D>();
+                _rb2d.freezeRotation = true;
+            }
+        }
+
+        protected override void CheckGrounded() {
+            _isGrounded = Physics2D.OverlapCircle(transform.position, 0.5f, _terrainLayer);
+        }
+
+    }
+
+}
--- Assets/UdeM/Controllers/EntityController.cs
+++ Assets/UdeM/Controllers/EntityController.cs
@@ -4,12 +4,34 @@ using UnityEngine;
 
 namespace UdeM.Controllers
 {
-    public class EntityController : MonoBehaviour
+    public abstract class EntityController : MonoBehaviour
     {
-        protected virtual void Awake () { }
+        protected float _speed;
+        protected float _jumpForce;
+        protected float _gravityMod;
+
+        protected LayerMask _terrainLayer;
+
+        [SerializeField] protected bool _isGrounded;
+
+
+        protected virtual void Awake() {
+            _speed = 10f;
+            _jumpForce = 10f;
+            _gravityMod = 1f;
+            _isGrounded = false;
+
+            _terrainLayer = LayerMask.NameToLayer("Terrain");
+            if ( _terrainLayer == -1) {
+                Debug.LogWarning("El layer de terreno no ha sido encontrado");
+            }
+
+        }
         protected virtual void Start () { }
         protected virtual void Update () { }
         protected virtual void FixedUpdate() { }
         protected virtual void LateUpdate() { }
+
+        protected abstract void CheckGrounded();
     }
 }
