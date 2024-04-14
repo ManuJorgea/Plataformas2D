140daec6e2456bd4b36ff6965b3b0234177dc508
ManuJorgea <69644681+ManuJorgea@users.noreply.github.com>
2024-04-14 16:04:05 -0500

    Menu Pausa y funciones en niveles

--- Assets/Scripts/CanvasController.cs
+++ Assets/Scripts/CanvasController.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class CanvasController : MonoBehaviour
 {
+    public void StartMenu()
+    {
+        SceneManager.LoadScene("Menu_Start_UI");
+    }
+
     public void PlayLevel01()
     {
         SceneManager.LoadScene("Level1");
@@ -18,5 +23,47 @@ public class CanvasController : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+
+    // Menú de pausa de nivel
+    ///Panel oculto
+    public GameObject menuPause;
+    public bool juegoPausado = false;
+
+    void Udpate(){
+
+        //Activación por medio del teclado
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            //Condición que revisa si el juego está o no en pausa
+            if (juegoPausado)
+            {
+                //Juego activo
+                Debug.Log("El juego está activo");
+                ContinuarJuego();
+            }
+            else
+            {
+                //Juego pausado
+                Debug.Log("El juego está pausado");
+                PausaJuego();
+            }
+        }
+
+    }
+
+    public void PausaJuego()
+    {
+        juegoPausado = true;
+        Time.timeScale = 0; //Pausar el juego
+        menuPause.SetActive(true);
+    }
+
+    public void ContinuarJuego()
+    {
+        juegoPausado = false;
+        Time.timeScale = 1;
+        menuPause.SetActive(false);
     }
 }
